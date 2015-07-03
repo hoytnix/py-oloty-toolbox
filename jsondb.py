6a0351a0f50ee81d9423caba8dc2841c2a21ac50
John Doe <me@idgaf.me>
2015-07-03 04:20:28 -0400

    jsondb => py3k

--- jsondb.py
+++ jsondb.py
@@ -1,43 +1,69 @@
+#!/usr/bin/env python
+
+'''
+py3k: True
+'''
+
 import json
 
 class Jsondb:
-  def __init__(self, f='db.json'):
-    self.f  = f
-    self.db = self.load()
+    def __init__(self, f='db.json', mode='r'):
+        self.f    = f
+        self.mode = mode
+        self.db = self.load()
 
-  def load(self):
-    with open(self.f, 'r') as _f:
-      data = json.load(_f)
-    return data
+        if self.db == None:
+            self.db = dict({})
 
-  def save(self):
-    with open(self.f, 'w') as _f:
-      data = json.dump(obj=self.db, fp=_f, indent=0)
-    return data
+    def load(self):
+        try:
+            with open(self.f, 'r') as _f:
+                data = json.load(_f)
+            return data
+        except ValueError:
+            if self.mode == 'w+':
+                return None
+            else:
+                return False
+        #except FileNotFoundError:
+        #    #return {}
+        #    return False
+        #except:
+        #    return False
 
-  def update(self, db):
-    self.db = db
+    def save(self):
+        with open(self.f, 'w') as _f:
+            data = json.dump(obj=self.db, fp=_f, indent=0)
+        return data
 
-  def _print(self):
-    data = json.dumps(obj=self.db, indent=4)
-    print data
+    def _print(self):
+        data = json.dumps(obj=self.db, indent=4)
+        print(data)
 
-  def add_key(self, key='lol'):
-    for o in self.db:
-      try:
-        self.db[o][key] = ''
-      except:
-        pass
+    def add_key(self, key='foo'):
+        for o in self.db:
+            try:
+                self.db[o][key] = ''
+            except:
+                pass
 
-def test():
-  j = Jsondb()
-  db = j.db
+def main():
+    db = Jsondb(f='/opt/datas/db.json', mode='w+')
+    json = db.db 
 
-  print len(db)
+    if json == False:
+        return False
 
-  #j.update(db)
-  #print j.db
-  #j.save()
+    print( json.__len__() )
+
+    json['foo'] = 'bar'
+
+    print( json.__len__() )
+
+    db.db = json
+    db.save()
+
+    return True
 
 if __name__ == '__main__':
-  test()
+    print( main() )
